e09880badae97ad2b4292a8146efa805aa441338
Matheus Pegorari <pegorari42@gmail.com>
2024-08-21 23:47:24 -0300

    first commit

---
+++ src/test/kotlin/dev/pegorari/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.pegorari.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
