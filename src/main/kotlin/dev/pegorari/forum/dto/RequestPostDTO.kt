effaf958bd32e73f982c0df90d8a0a5d81cf83fb
Matheus Pegorari <pegorari42@gmail.com>
2024-08-26 09:52:22 -0300

    feat: MethodArgumentNotValidException treatment

--- src/main/kotlin/dev/pegorari/forum/dto/RequestPostDTO.kt
+++ src/main/kotlin/dev/pegorari/forum/dto/RequestPostDTO.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class RequestPostDTO(
-    @field:Size(min = 5, max = 100, message = "Must be min 5 and max 100")
+    @field:Size(min = 5, max = 100)
     val title: String,
     @field:NotEmpty val message: String,
     @field:NotNull val courseId: Long,
--- src/main/kotlin/dev/pegorari/forum/infra/ExceptionHandler.kt
+++ src/main/kotlin/dev/pegorari/forum/infra/ExceptionHandler.kt
@@ -4,6 +4,8 @@ import dev.pegorari.forum.dto.ErrorDTO
 import dev.pegorari.forum.exception.NotFoundException
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.validation.method.MethodValidationException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -34,6 +36,21 @@ class ExceptionHandler {
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = e.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidation(
+        e: MethodArgumentNotValidException, request: HttpServletRequest
+    ): ErrorDTO {
+        val errorMessage = e.bindingResult.fieldErrors.associate { it.field to it.defaultMessage }
+        
+        return ErrorDTO(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
             path = request.servletPath
         )
     }
