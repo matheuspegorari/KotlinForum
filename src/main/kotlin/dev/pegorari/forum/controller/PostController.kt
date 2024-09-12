5bba27f9cae2276583ae26bc488994d0994bfad3
Matheus Pegorari <pegorari42@gmail.com>
2024-09-11 23:23:26 -0300

    feat: add caching

--- src/main/kotlin/dev/pegorari/forum/controller/PostController.kt
+++ src/main/kotlin/dev/pegorari/forum/controller/PostController.kt
@@ -6,6 +6,8 @@ import dev.pegorari.forum.dto.UpdatePostDTO
 import dev.pegorari.forum.service.PostService
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -20,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder
 class PostController(private val service: PostService) {
 
     @GetMapping
+    @Cacheable("posts")
     fun allPosts(
         @RequestParam(required = false) courseName: String?,
         @PageableDefault(size = 5, sort = ["createdAt"], direction = Sort.Direction.DESC)
@@ -35,17 +38,18 @@ class PostController(private val service: PostService) {
 
     @PostMapping
     @Transactional
+    @CacheEvict("posts", allEntries = true)
     fun create(
         @RequestBody @Valid dto: RequestPostDTO, uriBuilder: UriComponentsBuilder
     ): ResponseEntity<ResponsePostDTO> {
         val newPost = service.create(dto)
         val uri = uriBuilder.path("/posts/${newPost.id}").build().toUri()
         return ResponseEntity.created(uri).body(newPost)
-
     }
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict("posts", allEntries = true)
     fun update(
         @PathVariable id: Long,
         @RequestBody @Valid dto: UpdatePostDTO
@@ -56,6 +60,7 @@ class PostController(private val service: PostService) {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict("posts", allEntries = true)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun delete(@PathVariable id: Long) {
         service.delete(id)
